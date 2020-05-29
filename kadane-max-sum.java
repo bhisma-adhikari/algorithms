d4a29174823b3ea43f0d2e2bef7289a48b7d3992
ad718 <adhikari2718@gmail.com>
2020-05-29 19:45:28 -0400

    kadane-max-sum now returns both sum and indices

--- kadane-max-sum.java
+++ kadane-max-sum.java
@@ -1,6 +1,13 @@
 import java.lang.Math;
 
 class Program {
+    public static void main(String[] args) {
+        int[] ary = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        // int[] ary = { -2, -1, -3, -4, -1, -2, -1, -5, -4 };
+        System.out.println(kadaneMaxSum(ary));
+        System.out.println(kadaneMaxSumWithIndex(ary));
+    }
+
     /**
      * 
      * @param ary
@@ -20,9 +27,61 @@ class Program {
         return globalMax;
     }
 
-    public static void main(String[] args) {
-        int[] ary = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-        // int[] ary = { -2, -1, -3, -4, -1, -2, -1, -5, -4 };
-        System.out.println(kadaneMaxSum(ary));
+    public static Ans kadaneMaxSumWithIndex(int[] ary) throws RuntimeException {
+
+        // handle special cases first
+        if (ary.length == 0) {
+            throw new RuntimeException("Too short array");
+        } else if (ary.length == 1) {
+            return new Ans(ary[0], 0, 0);
+        }
+
+        // general case
+
+        int runningSum = ary[0];
+        int globalMax = ary[0];
+
+        int indexFrom = 0;
+        int indexTo = 0;
+
+        for (int i = 1; i < ary.length; i++) {
+            if (runningSum > 0) {
+                runningSum += ary[i];
+            } else {
+                if (ary[i] > runningSum) {
+                    runningSum = ary[i];
+                    indexFrom = i;
+                }
+            }
+
+            if (runningSum > globalMax) {
+                globalMax = runningSum;
+                indexTo = i;
+            }
+        }
+
+        if (indexTo < indexFrom) {
+            indexTo = indexFrom;
+        }
+
+        return new Ans(globalMax, indexFrom, indexTo);
+    }
+
+}
+
+class Ans {
+    public int sum;
+    public int indexFrom;
+    public int indexTo;
+
+    public Ans(int sum, int indexFrom, int indexTo) {
+        this.sum = sum;
+        this.indexFrom = indexFrom;
+        this.indexTo = indexTo;
+    }
+
+    @Override
+    public String toString() {
+        return "sum : " + sum + "\nindexFrom : " + indexFrom + "\nindexTo : " + indexTo;
     }
 }

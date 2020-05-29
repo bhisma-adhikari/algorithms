fdda96e6befa61e8de3b728eabe8d0e0a41f256d
ad718 <adhikari2718@gmail.com>
2020-05-29 14:46:08 -0400

    addd kadane-max-sum.java

---
+++ kadane-max-sum.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+
+class Program {
+    /**
+     * 
+     * @param ary
+     * @return Maximum possible sum of any subarray 
+     */
+    public static double kadaneMaxSum(int[] ary){
+        double localMax = Double.NEGATIVE_INFINITY; 
+        double globalMax = Double.NEGATIVE_INFINITY; 
+
+
+        for (int i=0; i<ary.length; i++){
+            localMax = Math.max(ary[i], localMax + ary[i]); 
+            // System.out.println(localMax);
+            if (localMax > globalMax){
+                globalMax = localMax; 
+            } 
+        }
+        return globalMax;
+    }
+
+    public static void main(String[] args) {
+        int[] ary = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        // int[] ary = { -2, -1, -3, -4, -1, -2, -1, -5, -4 };
+        System.out.println(kadaneMaxSum(ary));
+
+    }
+}
